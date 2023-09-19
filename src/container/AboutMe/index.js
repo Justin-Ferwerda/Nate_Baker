751c496d846c9b831ae9559a004fa1adc3960553
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-09-19 10:19:45 -0500

    added my reel page

--- src/container/AboutMe/index.js
+++ src/container/AboutMe/index.js
@@ -2,21 +2,20 @@ import React from 'react';
 import {Container, Row, Col} from 'react-bootstrap'
 import Thumbnail from "../../components/thumbnail";
 import Content from "../../components/content";
-import Skill from "../../components/skill";
 import aboutData from '../../data/about-me'
 
 const AboutMe = ({type}) => {
     return (
         <div className={type !== 'page' ? 'section-padding section' : null}>
             <Container>
-                <Row className={'align-items-center'}>
+                <Row /* className={'align-items-center'} */>
                     <Col lg={6}>
                         <Thumbnail classes={'about-thumbnail mb-md-30 mb-sm-30 mb-xs-30'} thumb={`about/${aboutData.thumb}`}/>
                     </Col>
 
                     <Col lg={6}>
                         <Content classes={'about-content'}>
-                            {type !== 'page' ? (<h3 className="block-title">ABOUT ME</h3>) : null}
+                            <h3 className="block-title">ABOUT ME</h3>
                             <p>{aboutData.bio}</p>
                             <ul className="personal-info">
                                 <li><span>Skype:</span>{aboutData.skype}</li>
@@ -28,18 +27,6 @@ const AboutMe = ({type}) => {
                                     ))}
                                 </li>
                             </ul>
-
-                            <h3 className="block-title">Skill</h3>
-
-                            <div className="skill-wrap">
-                                {aboutData.skills.map(skill => (
-                                    <Skill
-                                        key={skill.id}
-                                        title={skill.title}
-                                        percentage={skill.percentage}
-                                    />
-                                ))}
-                            </div>
                         </Content>
                     </Col>
                 </Row>
