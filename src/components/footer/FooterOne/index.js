f5cf682940a8aa73fd71d44575537470b52523f3
Justin Ferwerda <justin.ferwerda@gmail.com>
2023-09-18 19:02:16 -0500

    changed footer to remove search bar and do copyright

--- src/components/footer/FooterOne/index.js
+++ src/components/footer/FooterOne/index.js
@@ -6,7 +6,7 @@ const FooterOne = ({next, prev}) => {
             <div className="container-fluid">
                 <div className="row align-items-center">
                     <div className="footer-copyright col-lg-4 col-12 order-lg-1 order-3">
-                        <p>&copy; {new Date().getFullYear()} MINIMAU. ALL RIGHT RESERVED</p>
+                        <p>&copy; {new Date().getFullYear()} Nate Baker.  ALL RIGHTS RESERVED</p>
                     </div>
 
                     <div className="col-lg-4 col-12 order-lg-2 order-1 mb-sm-15 mb-xs-15">
@@ -22,13 +22,6 @@ const FooterOne = ({next, prev}) => {
                                 onClick={next}>next
                             </button>
                         </div>
-                    </div>
-
-                    <div className="footer-search col-lg-4 col-12 order-lg-3 order-2 mb-sm-15 mb-xs-15">
-                        <form action="#">
-                            <input type="text" placeholder="SEARCH NOW"/>
-                            <button><i className="fa fa-search"/></button>
-                        </form>
                     </div>
                 </div>
             </div>
--- src/components/footer/FooterTwo/index.js
+++ src/components/footer/FooterTwo/index.js
@@ -6,7 +6,7 @@ const FooterTwo = ({loadHandler, allItems}) => {
             <div className="container-fluid">
                 <div className="row align-items-center">
                     <div className="footer-copyright col-lg-4 col-12 order-lg-1 order-3">
-                        <p>&copy; {new Date().getFullYear()} MINIMAU. ALL RIGHT RESERVED</p>
+                        <p>&copy; {new Date().getFullYear()} Nate Baker.  ALL RIGHTS RESERVED</p>
                     </div>
 
                     <div className="col-lg-4 col-12 d-flex justify-content-center order-lg-2 order-1 mb-sm-15 mb-xs-15">
@@ -18,13 +18,6 @@ const FooterTwo = ({loadHandler, allItems}) => {
                         >
                             LOAD MORE...
                         </button>
-                    </div>
-
-                    <div className="footer-search col-lg-4 col-12 order-lg-3 order-2 mb-sm-15 mb-xs-15">
-                        <form action="#">
-                            <input type="text" placeholder="SEARCH NOW"/>
-                            <button><i className="fa fa-search"/></button>
-                        </form>
                     </div>
                 </div>
             </div>
