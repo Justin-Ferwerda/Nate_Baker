550cc99b86e2597b8894bb37afa3e5d91acb509d
Justin Ferwerda <justin.ferwerda@gmail.com>
2025-04-14 10:53:20 -0500

    fixed video detail bug

--- src/pages/portfolio/details/index.js
+++ src/pages/portfolio/details/index.js
@@ -1,36 +1,12 @@
 import React, {Fragment} from 'react';
 import SideHeader from "../../../components/SideHeader";
+import portfolios from "../../../data/portfolios"
 import useGetParams from "../../../hooks/useGetParams";
 import Header from "../../../components/header/HeaderOne";
 import FooterThree from "../../../components/footer/FooterThree";
 import TemplatePortfolioDetails from "../../../templates/portfolio/details";
 
-const portfolios = [
-      {
-        "id": 1,
-        "title": "Nate Baker Reel",
-        "slug": "my-reel",
-        "category": "",
-        "thumb": "my-reel.jpg",
-        "description": "Covering mostly my time before working in baseball, I shot everything in my reel, created all the animations, and edited all sequences. I also acted as Director or Assistant Director for everything included. Some restrictions prohibit me from including my work with the Washington Nationals but please see my portfolio page for some of my complete work in sports.",
-        "previewImages": [
-          "Pr61tH5oU2E"
-        ]
-      },
-      {
-        "id": 2,
-        "title": "Nate Baker Reel",
-        "slug": "my-reel",
-        "category": "",
-        "thumb": "my-reel.jpg",
-        "description": "Covering mostly my time before working in baseball, I shot everything in my reel, created all the animations, and edited all sequences. I also acted as Director or Assistant Director for everything included. Some restrictions prohibit me from including my work with the Washington Nationals but please see my portfolio page for some of my complete work in sports.",
-        "previewImages": [
-          "Pr61tH5oU2E"
-        ]
-      }
-    ]
-
-const PortfolioDetailsPage = () => {
+const VideoDetailsPage = () => {
     const {slug} = useGetParams();
     const portfolio = portfolios?.find(portfolio => portfolio.slug === slug);
 
@@ -44,4 +20,4 @@ const PortfolioDetailsPage = () => {
     );
 };
 
-export default PortfolioDetailsPage;
+export default VideoDetailsPage;
